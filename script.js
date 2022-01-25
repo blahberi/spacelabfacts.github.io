1c9a2808785af25a570d8bdc7eb643d6a02942de
kamoodi <96086219+kamoodi@users.noreply.github.com>
2022-01-25 23:43:29 +0200

    updating it so it can use image 16

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function get_facts(){
 
 facts = get_facts();
 
-file = getRandomInt(1, 16);
+file = getRandomInt(1, 17);
 fact = facts[getRandomInt(0, facts.length)];
 document.body.style.backgroundImage = "url('images/"+file.toString()+".jpeg')";
 
